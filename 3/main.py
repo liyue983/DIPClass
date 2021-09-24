55aefd1aa1ba2d5ff06d488ec9490f21775300ee
liyue983 <liyue983@126.com>
2021-09-24 20:41:55 +0800

    Initial

---
+++ 3/main.py
@@ -0,0 +1,50 @@
+import cv2
+import os
+import numpy as np
+from skimage.measure import compare_mse
+from skimage.measure import compare_psnr
+from skimage.measure import compare_ssim
+from skimage.measure import compare_nrmse
+from skimage.measure import shannon_entropy
+
+def load_images_from_folder(fd):
+    imgs = []
+    for filename in os.listdir(fd):
+        img = cv2.imread(os.path.join(fd,filename))
+        if (img is not None):
+            imgs.append(togray(img))
+    return imgs
+
+def togray(img):
+    return cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+def main():
+    originpath = "red.png"
+    targetfolder = "dev"
+    origin = togray(cv2.imread(originpath))
+    targetimgs = load_images_from_folder(targetfolder)
+
+    for i in range(len(targetimgs)):
+        print("="*10,"image number:",i,"="*10)
+        mse = compare_mse(origin,targetimgs[i])
+        print(f'MSE:{mse}')
+        psnr = compare_psnr(origin,targetimgs[i])
+        print(f'PSNR:{psnr}')
+        ssim = compare_ssim(origin,targetimgs[i])
+        print(f'SSIM:{ssim}')
+        nrmse = compare_nrmse(origin,targetimgs[i])
+        print(f'NRMSE:{nrmse}')
+        entropy = shannon_entropy(targetimgs[i])
+        print(f'Entropy:{entropy}')
+
+    ht = np.hstack([origin,*targetimgs])
+    cv2.imshow('images',ht)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+  
+
+
+
+if __name__=="__main__":
+    main()
