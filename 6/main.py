ebdd6e4aa7b0dd674c1b7b0c68924e5f7bd29db3
liyue983 <liyue983@126.com>
2021-10-15 20:23:48 +0800

    update

--- 6/main.py
+++ 6/main.py
@@ -6,9 +6,9 @@ def gray2color(gr):
     elif 50<=gr<100:
         return (50,50,50)
     elif 100<=gr<200:
-        return (220,70,0)#[::-1]
+        return (220,70,0)[::-1]
     elif 200<=gr<240:
-        return (240,240,90)#[::-1]
+        return (240,240,90)[::-1]
     return (255,255,255)
 
 
@@ -16,15 +16,17 @@ def gray2color(gr):
 img = cv2.imread("img.png")
 img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 img_color = cv2.applyColorMap(img,cv2.COLORMAP_HOT)
+img_color2 = cv2.cvtColor(img_gray,cv2.COLOR_GRAY2BGR)
 
 w = img_gray.shape[0]
 h = img_gray.shape[1]
 for i in range(w):
     for j in range(h):
-        img_color[i][j]=gray2color(img_gray[i][j])
+        img_color2[i][j]=gray2color(img_gray[i][j])
 
 cv2.imshow("gray",img_gray)
 cv2.imshow("color",img_color)
+cv2.imshow("color2",img_color2)
 cv2.waitKey(0)
 
 
