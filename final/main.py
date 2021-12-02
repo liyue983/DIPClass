576830bc9defef0392b8b8ed01f8069478a7bd71
liyue983 <liyue983@126.com>
2021-12-02 13:59:16 +0800

    backup

--- final/main.py
+++ final/main.py
@@ -63,6 +63,8 @@ fshift = dft_shift * mask
 # fshift[int(rows / 2 - m):int(rows / 2 + m), int(cols / 2 - l):int(cols / 2 + l)]=0
 
 f2 = cv2.magnitude(fshift[:, :, 0], fshift[:, :, 1])
+# f2[:, 80:100][20*np.log(f2[:, 80:100]+1)>150]=(math.e**(100/20))
+# f2[:, 255:275][20*np.log(f2[:, 255:275]+1)>150]=(math.e**(100/20))
 # cv2.imshow("fft2",do_math(f2,lambda x:math.log(abs(x)+1))/16)
 plt.subplot(122),plt.imshow(20*np.log(f2+1),cmap='gray')
 
@@ -70,23 +72,52 @@ f_ishift = np.fft.ifftshift(fshift) # fftshit()函数的逆函数，它将频谱
 img_back = cv2.idft(f_ishift)/(rows*cols) # 将频率域转化回空间域，输出是一个复数，cv2.idft()返回的是一个双通道图像
 img_back2 = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1]) # idft[:,:,0]求得实部，用idft[:,:,1]求得虚部。
 
+plt.figure()
+# plt.hist(20*np.log(f[:, 80:100]+1).ravel(),512)
+localf = f[:, 80:100].copy()
+plt.subplot(221),plt.hist(20*np.log(localf+1).ravel(),512)
+plt.subplot(223),plt.imshow(20*np.log(localf+1),cmap='gray')
+localf[20*np.log(localf+1)>140]=(math.e**(100/20))
+plt.subplot(222),plt.hist(20*np.log(localf+1).ravel(),512)
+plt.subplot(224),plt.imshow(20*np.log(localf+1),cmap='gray')
+
 ##直方图
 plt.figure()
-plt.hist(img_back2.ravel(), 256)
+plt.hist((np.uint8(img_back2)).ravel(), 256)
 
 ##局部直方图均衡
 img_back3 = np.uint8(img_back2)
 clahe = cv2.createCLAHE(clipLimit=5, tileGridSize=(23,23))
 img_back3 = clahe.apply(img_back3)
-
+# plt.hist((np.uint8(img_back3)).ravel(), 256)
 
 ##图像增强
-img_back4 = np.uint8(img_back2*1.5)
+img_back4 = np.uint8(img_back2)
 kernel_1 = np.array([[-1,-1,-1],
                         [-1,9,-1],
                         [-1,-1,-1]])
 img_back4 = cv2.filter2D(img_back4,-1,kernel_1)
 cv2.imshow('c',(img_back4))
+
+##试一试对比度拉伸
+def stretch(x):
+    x1=70
+    x2=140
+    y1=40
+    y2=245
+    if x<=0:return 0 
+    if x>=255:return 255
+    if x<=x1:
+        return x*y1/x1
+    elif x1<x<=x2:
+        return (x-x1)*(y2-y1)/(x2-x1)+y1
+    else:
+        return (x-x2)*(255-y2)/(255-x2)+y2
+img_back5 = img_back2.copy()
+img_back5 = do_math(img_back5,stretch)
+plt.hist((np.uint8(img_back5)).ravel(), 256)
+cv2.imshow('stretch',np.uint8(img_back5))
+
 
 plt.figure()
 plt.subplot(131), plt.imshow(img, cmap='gray')
